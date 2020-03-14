95874a88e0ed2fa451ab64e44a5af672a1d1ebca
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-03-14 23:49:02 +0100

    Add preview image

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use graphics_buffer::{RenderBuffer, IDENTITY};
 use piston_window::rectangle::*;
 use piston_window::types::Scalar;
 use piston_window::*;
 
 fn main() {
+    let mut buffer = RenderBuffer::new(640, 480);
     let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
         .exit_on_esc(true)
         .build()
@@ -10,6 +12,7 @@ fn main() {
     while let Some(event) = window.next() {
         window.draw_2d(&event, |context, graphics, _device| {
             clear([1.0; 4], graphics);
+            clear([1.0; 4], &mut buffer);
             for i in 0..9 {
                 let dims = square(i as Scalar * 50.0, 0.0, 50.0);
                 let rectangle = Rectangle::new([1.0, 0.0, 0.0, 1.0]).border(Border {
@@ -23,7 +26,14 @@ fn main() {
                     context.transform,
                     graphics,
                 );
+                rectangle.draw(
+                    dims,
+                    &draw_state::DrawState::default(),
+                    IDENTITY,
+                    &mut buffer,
+                );
             }
         });
     }
+    buffer.save("resources/output.png").unwrap();
 }
